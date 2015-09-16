b4dc42b03276082648ac028e7cdf02ae567957d9
xingfukun <874898417@qq.com>
2015-09-16 19:15:52 +0800

    update

---
+++ source/KVCObject.h
@@ -0,0 +1,18 @@
+//
+//  KVCObject.h
+//  KVCKVO
+//
+//  Created by joymain on 15/9/15.
+//  Copyright (c) 2015年 邢富琨. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface KVCObject : NSObject
+{
+    NSString *param1;
+    
+    UILabel *testLabel;
+}
+@end
